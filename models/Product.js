3246b007c794d02032ee02f36886c8855bc122dc
Mohamed Sidow <sidowmohamed35@gmail.com>
2023-02-22 14:26:11 +0000

    finished database models and index.js for associations

--- models/Product.js
+++ models/Product.js
@@ -12,23 +12,42 @@ Product.init(
     // define columns
     id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
+      allowNull:false,
+      primaryKey:true,
+      autoIncrement:true,
     },
     
     product_name: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull:false,
     },
 
     price: {
       type: DataTypes.DECIMAL,
+      allowNull:false,
+      validate: {
+        isDecimal:true,
+       }
+    },
+
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull:false,
+      defaultValue:10,
+      validate: {
+        isNumeric:true,
+       }
+    },
+    
+    category_id: {
+      type: DataTypes.INTEGER,
       allowNull: false,
-     validate: {
-      isDecimal:true,
-     }
-    }
+      references: {
+        model: 'Category',
+        key: 'id',
+      },
+    },
+
   },
   {
     sequelize,
