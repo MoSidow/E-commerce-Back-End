5eaa7c133ec29edf0a3b4425aabdd092a4af1008
Mohamed Sidow <sidowmohamed35@gmail.com>
2023-02-23 18:45:06 +0000

    fixed category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Products
   try {
     const CategoryData = await Category.findAll({
-      include: [{ Product }],
+      include: [{ model: Product }],
     });
     res.status(200).json(CategoryData);
   } catch (err) {
@@ -21,7 +21,7 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Products
   try {
     const CategoryData = await Category.findByPk(req.params.id, {
-      include: [{ Product }],
+      include: [{ model: Product }],
     });
 
     if (!CategoryData) {
